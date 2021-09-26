c0ec06ae33e94f9ff07102612dd5ad3d0d25f2d7
Михаил <191egor1911@mail.ru>
2021-09-26 21:50:25 +0300

    testGithub

---
+++ src/test/java/com/Hrizantemovich/SelenideTest.java
@@ -0,0 +1,27 @@
+package com.Hrizantemovich;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byPartialLinkText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+    @Test
+    void testGithub() {
+        open("https://github.com/");
+        $(".header-search-input").setValue("eroshenkoam/allure-example").pressEnter();
+        $(By.linkText("eroshenkoam/allure-example")).click();
+        $(byPartialLinkText("Issues")).click();
+        $("#issue_68_link").shouldHave(text("Listeners NamedBy"));
+
+
+    }
+
+}
+
