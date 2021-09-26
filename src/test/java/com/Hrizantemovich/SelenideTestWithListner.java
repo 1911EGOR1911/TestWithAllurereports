5829c65a877eb014b14cb5c18118c0df3ca4f05f
Михаил <191egor1911@mail.ru>
2021-09-26 23:03:20 +0300

    testGithubWithListner

--- src/test/java/com/Hrizantemovich/SelenideTestWithListner.java
+++ src/test/java/com/Hrizantemovich/SelenideTestWithListner.java
@@ -2,9 +2,6 @@ package com.Hrizantemovich;
 
 
 
-
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -13,11 +10,9 @@ import static com.codeborne.selenide.Selectors.byPartialLinkText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SelenideTest {
-
+public class SelenideTestWithListner {
     @Test
     void testGithub() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
         open("https://github.com/");
         $(".header-search-input").setValue("eroshenkoam/allure-example").pressEnter();
         $(By.linkText("eroshenkoam/allure-example")).click();
